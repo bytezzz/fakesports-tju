078685f05074866b25b738d8dec92967a349e68e
Higgs <veweew@outlook.com>
2020-12-01 08:19:30 +0800

    Initial Commit

---
+++ loc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  loc
+//
+//  Created by Eric Zhao on 2020/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
